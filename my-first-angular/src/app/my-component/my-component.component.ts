1c894e3e8eb46acbe0bdd0937fea3ff11dfc6ad1
rscarpi_prest-at-493093903190 <you@example.com>
2020-09-22 08:58:24 -0300

    commit 2

---
+++ my-first-angular/src/app/my-component/my-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.scss']
+})
+export class MyComponentComponent implements OnInit {
+
+  hello_world = "HELLO WORLD"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
