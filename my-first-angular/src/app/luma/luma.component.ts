75a415a9298d50b3d66a1a00ee21e1eab55ed707
luma-chen-ey <Luma.Chen@br.ey.com>
2020-09-28 19:58:18 -0300

    for realizado com sucesso

--- my-first-angular/src/app/luma/luma.component.ts
+++ my-first-angular/src/app/luma/luma.component.ts
@@ -1,17 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { MyappService } from '../myapp.service';
 
-@Component({
+@Component
+({
   selector: 'app-luma',
   templateUrl: './luma.component.html',
   styleUrls: ['./luma.component.scss']
 })
-export class LumaComponent implements OnInit {
+export class LumaComponent implements OnInit 
+{
+  estados: any = this.getEstados();
+  myName= "Luma Chen";
+  http: any;
 
-  meu_nome = "Luma Chen"
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private myappService: MyappService) 
+  {
   }
 
+  ngOnInit(): void //executa quando abre o componente
+  {
+    for (let i= 0; i < this.estados.length; i++)
+    {
+      let estado= this.estados[i];
+    }
+  }
+  
+  getEstados ()
+  {
+    this.myappService.getEstadosRequest().subscribe 
+    (
+      sucesso =>
+      {
+        this.estados= sucesso;
+        console.log (this.estados);
+      },
+      erro =>
+      {
+       console.log(erro);
+      }
+    ) 
+  }
 }
